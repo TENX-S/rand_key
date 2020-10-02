91e2d82ad35bb135655d519eb08c0767ffbf52f1
TEN.X <40336192+TenX01@users.noreply.github.com>
2020-10-02 23:33:41 +0800

    add invalid test

--- tests/error_test.rs
+++ tests/error_test.rs
@@ -1,11 +1,34 @@
 
-#[cfg(tests)]
-mod test {
+#![allow(non_snake_case)]
+
+
+#[cfg(test)]
+mod InvaidInput {
+
+    use rand_key::RandKey;
 
     #[test]
+    #[should_panic]
     fn test_1() {
-
+        let mut r_p = RandKey::new("A", "B", "C").unwrap();
+        r_p.join().unwrap();
     }
 
+    #[test]
+    #[should_panic]
+    fn test_2() {
+        let mut r_p = RandKey::new("-1", "0", "0").unwrap();
+        r_p.join().unwrap();
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_3() {
+        let mut r_p = RandKey::new("你好", "1", "C").unwrap();
+        r_p.join().unwrap();
+    }
+
+
 }
+
 
