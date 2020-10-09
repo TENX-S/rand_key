2fadaa1a177b03641d9b83589e6fbe57200b9150
TENX-S <ttenx@pm.me>
2020-10-09 20:12:43 +0800

    remove unused

--- tests/error_test.rs
+++ tests/error_test.rs
@@ -9,25 +9,21 @@ mod InvalidInput {
     #[test]
     #[should_panic]
     fn test_1() {
-        let mut r_p = RandKey::new("A", "B", "C").unwrap();
+        let r_p = RandKey::new("A", "B", "C").unwrap();
         r_p.join().unwrap();
     }
 
     #[test]
     #[should_panic]
     fn test_2() {
-        let mut r_p = RandKey::new("-1", "0", "0").unwrap();
+        let r_p = RandKey::new("-1", "0", "0").unwrap();
         r_p.join().unwrap();
     }
 
     #[test]
     #[should_panic]
     fn test_3() {
-        let mut r_p = RandKey::new("你好", "1", "C").unwrap();
+        let r_p = RandKey::new("你好", "1", "C").unwrap();
         r_p.join().unwrap();
     }
-
-
 }
-
-
